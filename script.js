6f79c09c1975160b95ff94fcb16998cb93325365
Elton017 <141003173+Elton017@users.noreply.github.com>
2024-11-11 07:53:21 +1030

    script logic added

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 const container = document.querySelector(".container");
 
-let rows = 16;
-let columns = 16;
+let gridSize = 16;
 
-for (let i=1; i < rows+1; i++){
-    let div = document.createElement("div");
-    div.className += "row";
-    container.appendChild(div)
-    for (let j=1; j < columns+1; j++) {
-        let cell = document.createElement("div");
-        cell.className += "cell";
-        div.appendChild(cell);
+function createGrid(size) {
+    container.innerHTML=""
+    for (let i=1; i < size+1; i++){
+        let div = document.createElement("div");
+        div.className += "row";
+        container.appendChild(div)
+        for (let j=1; j < size+1; j++) {
+            let cell = document.createElement("div");
+            cell.className += "cell";
+            div.appendChild(cell);
+        }
     }
 }
+
+createGrid(gridSize)
+
+container.addEventListener("mouseover", (e) => {
+    if (e.target.className == "cell"){
+        e.target.style.backgroundColor = "black";
+        let opacity = parseFloat(e.target.style.opacity) || 0;
+        opacity += 0.1;
+        e.target.style.opacity = opacity
+    }
+})
+
+let gridChanger = document.querySelector(".gridsize");
+gridChanger.addEventListener("input", (e) => {
+    createGrid(parseInt(e.target.value));
+})
